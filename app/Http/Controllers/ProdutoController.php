6efbcc3f3d790af1c04040354486ef5192528d0a
Lucas Santos <10000863+imlucassantos@users.noreply.github.com>
2019-04-01 14:00:04 -0300

    First Commit
    Trabalhando com rotas

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -1,7 +1,22 @@
 <?php namespace estoque\Http\Controllers;
+use Request;
+use Illuminate\Support\Facades\DB;
+use estoque\Produto;
 
 class ProdutoController extends Controller {
+
     public function lista(){
-        return 'Lista';
+        $produtos = DB::select('select * from produtos');
+        return view('listagem')->with('produtos', $produtos);
+    }
+
+    public function  mostra($id){
+
+        $resposta = DB::select('select * from produtos where id = ?', [$id]);
+
+        if(empty($resposta)) {
+            return "Esse produto não existe";
+        }
+        return view('detalhes')->with('p', $resposta[0]);
     }
 }
